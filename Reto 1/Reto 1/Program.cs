c500d54fb830cbd264c504c865a512329b9108f4
Zorritx <133102150+alkagam@users.noreply.github.com>
2024-06-28 17:41:14 -0600

    Add files via upload

---
+++ Reto 1/Reto 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+class Programa
+{
+    static List<string> FizzBuzz(int n)
+
+    {
+        List<string> result = new List<string>();
+        for (int i = 1; i <= n; ++i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                result.Add("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                result.Add("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                result.Add("Buzz");
+            }
+            else
+            {
+                result.Add(i.ToString() + "");
+            }
+        }
+        return result;
+    }
+    static void Main(string[] args)
+    {
+        int n = 100;
+        Console.WriteLine("0");
+        List<string> result = FizzBuzz(n);
+        
+        foreach(string s in result) 
+        {
+            Console.Write(s + "  \n");
+        }
+    }
+
+
+}
